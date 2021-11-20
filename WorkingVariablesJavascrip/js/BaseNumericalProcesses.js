682f2706bdfb794c5dacc16a205e81c62a019b13
ahmetmujde <mujde.ahmet79@gmail.com>
2021-11-20 15:18:44 +0300

    [WORKINGVARIABLESJAVASCRIPT] Following features are developed. - BaseNumericalProcesses.js

---
+++ WorkingVariablesJavascrip/js/BaseNumericalProcesses.js
@@ -0,0 +1,42 @@
+
+let  x = 3;
+let  y = 6;
+let  z = 0;
+// add process
+z = x + y
+console.log(x, y)
+console.log("x + y = " + z)
+y += 5
+x++
+z += 1
+console.log(x, y, z)
+
+//subtraction process
+z = y - x
+console.log(x, y)
+console.log("x + y = " + z)
+y -= 2
+x--
+z -= 2 
+console.log(x, y, z)
+
+//multiplication process
+z = y * x
+console.log(x, y)
+console.log("x * y = " + z)
+y = y * 2
+z *= 2 
+console.log(x, y, z)
+
+//divide process
+z = y / x
+console.log(x, y)
+console.log("x / y = " + z)
+y = y / 2
+x /= 2 
+console.log(x, y, z)
+
+// Modular Arithmetic  Prosses
+y = 10
+z = 3
+console.log("y % z  = "+ (y % z)) 
