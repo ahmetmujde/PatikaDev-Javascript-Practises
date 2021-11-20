5f2a5230f8029508fc2e8f128cfd82081eaeb7ad
ahmetmujde <mujde.ahmet79@gmail.com>
2021-11-20 13:03:23 +0300

    first commit

---
+++ WorkingVariablesJavascrip/js/HelloWorld.js
@@ -0,0 +1,9 @@
+
+//show the screen
+document.write("Hello  World");
+
+//print with allert messages 
+alert("hello world")
+
+//print the console
+console.log("hello world")
---
+++ WorkingVariablesJavascrip/js/VariableDefinition.js
@@ -0,0 +1,36 @@
+
+//variable definition  with var 
+var nameValue= "ahmet"
+console.log(nameValue)
+
+//variable definition with let but assignment value is null
+let anyValue;
+console.log(anyValue)
+
+
+//variable definition with let and assignment value to variable
+let townName = "New York"
+console.log(townName)
+
+let townNameStreet = "8th Street and St. Mark's Place.";
+console.log(townNameStreet)
+
+//change value in variable
+townNameStreet = "14th Street (Manhattan)"
+console.log(townNameStreet)
+
+
+//concatenation  or added operation
+townNameStreet = "4th Street (Manhattan)" //assignment to new value
+console.log(townNameStreet + ", 8th Street and St. Mark's Place.") // not assigntment, just showing with concatenation
+
+townNameStreet = townNameStreet + ", 8th Street and St. Mark's Place" //assignment to new value
+townNameStreet += ", 14th Street (Manhattan)." //assignment to new value
+console.log(townNameStreet)
+
+
+//variable definition with const and assignment value to variable
+const SERVER_NAME = "PATİKA"
+console.log(SERVER_NAME)
+
+
